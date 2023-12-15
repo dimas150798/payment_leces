d54c3e806b100e675186b056a4a579bab8be6497
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-12-15 09:26:16 +0700

    Menambah tahun sampai 2025

--- application/views/admin/BelumLunas/V_BelumLunas.php
+++ application/views/admin/BelumLunas/V_BelumLunas.php
@@ -49,7 +49,7 @@ if (!function_exists('changeDateFormat')) {
                                 if ($tahunGET == NULL) {
                                     echo '<option value="" disabled selected>-- Pilih Tahun --</option>';
 
-                                    for ($i = 2022; $i <= 2023; $i++) {
+                                    for ($i = 2022; $i <= 2025; $i++) {
                                         if ($tahun == $i) {
                                             echo '<option selected value=' . $i . '>' . date("Y", mktime(0, 0, 0, 1, 1, $i)) . '</option>' . "\n";
                                         } else {
@@ -59,7 +59,7 @@ if (!function_exists('changeDateFormat')) {
                                 } else {
                                     echo '<option value="" disabled>-- Pilih Tahun --</option>';
 
-                                    for ($i = 2022; $i <= 2023; $i++) {
+                                    for ($i = 2022; $i <= 2025; $i++) {
                                         if ($tahunGET == $i) {
                                             echo '<option selected value=' . $i . '>' . date("Y", mktime(0, 0, 0, 1, 1, $i)) . '</option>' . "\n";
                                         } else {
--- application/views/admin/SudahLunas/V_SudahLunas.php
+++ application/views/admin/SudahLunas/V_SudahLunas.php
@@ -36,7 +36,7 @@ if (!function_exists('changeDateFormat')) {
                                 if ($tahunGET == NULL) {
                                     echo '<option value="" disabled selected>-- Pilih Tahun --</option>';
 
-                                    for ($i = 2022; $i <= 2023; $i++) {
+                                    for ($i = 2022; $i <= 2025; $i++) {
                                         if ($tahun == $i) {
                                             echo '<option selected value=' . $i . '>' . date("Y", mktime(0, 0, 0, 1, 1, $i)) . '</option>' . "\n";
                                         } else {
@@ -46,7 +46,7 @@ if (!function_exists('changeDateFormat')) {
                                 } else {
                                     echo '<option value="" disabled>-- Pilih Tahun --</option>';
 
-                                    for ($i = 2022; $i <= 2023; $i++) {
+                                    for ($i = 2022; $i <= 2025; $i++) {
                                         if ($tahunGET == $i) {
                                             echo '<option selected value=' . $i . '>' . date("Y", mktime(0, 0, 0, 1, 1, $i)) . '</option>' . "\n";
                                         } else {
